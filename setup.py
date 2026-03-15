c384e092f1e85d0131c4d4edc3ca9e0c0bfedaf8
Jaynee Rawal <awesdewas@gmail.com>
2026-03-15 22:26:23 +0800

    Add package distribution config

---
+++ setup.py
@@ -0,0 +1,1 @@
+from setuptools import setup, find_packages\n\nsetup(\n    name="smart-groundwater-monitor",\n    version="1.0.0",\n    packages=find_packages(),\n    install_requires=open('requirements.txt').read().splitlines(),\n    author="Jaynee Rawal",\n    description="Smart Groundwater Monitor - Professional AI Research Component",\n)\n
